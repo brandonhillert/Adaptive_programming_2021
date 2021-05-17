dd2a89b55612eaa058f7b3e466a1cf1342f361d3
Brandon Hillert <brandon.hillert@student.hu.nl>
2021-05-17 19:50:17 +0200

    Commentaar

--- formatieve opdracht 2a machine met tekstinvoer/src/ObjectOrientedProgramming/Main.java
+++ formatieve opdracht 2a machine met tekstinvoer/src/ObjectOrientedProgramming/Main.java
@@ -1,3 +1,9 @@
+/**
+ * Author: Brandon Hillert
+ * Date: 17-05-2021
+ * Program: Finite State Machine with Text implement
+ */
+
 package ObjectOrientedProgramming;
 import java.util.Scanner;
 
@@ -6,16 +12,18 @@ public class Main {
     public static <code> void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        
+
+        //Aanmaken Node objecten
         Node s0 = new Node("S0");
         Node s1 = new Node("S1");
         Node s2 = new Node("S2");
         Node s3 = new Node("S3");
 
+
         Node state = s0;
         String code;
 
-
+        //Functie voor het invullen van een geldige code
         do {
             System.out.println("Geef een code dat bestaat uit 4 characters allen A of B: ");
             code = input.next();
@@ -23,11 +31,13 @@ public class Main {
         while (!checkCode(code));
 
 
+        // If statement die het patroon van de code volgt
         if (checkCode(code)) {
             for (int i = 0; i < 4; i++) {
 
                 String connection = Character.toString(code.charAt(i));
 
+                //Letter A in code geeft transities A
                 if (connection.equals("A")) {
                     s0.setNextNode(s2); //A
                     s1.setNextNode(s1); //A
@@ -39,6 +49,7 @@ public class Main {
                     }
                 }
 
+                //Letter A in code geeft transities A
                 if (connection.equals("B")) {
                     s0.setNextNode(s1); //B
                     s1.setNextNode(s2); //B
@@ -54,6 +65,7 @@ public class Main {
         }
     }
 
+    // Functie die code controleert op format
     private static boolean checkCode(String codes){
         String code = codes;
         boolean codeGoedOfFout = false;
